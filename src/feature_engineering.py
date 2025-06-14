255982da694b47e552636f8eefac5ef0ad4c6a25
callumanderson <callum.g.anderson@gmail.com>
2025-06-14 18:47:19 -0400

    melt code to merge into the final csv before feature engineering

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -19,10 +19,5 @@ from src import utils
 """
 
 
-
-# TODO: melt all csvs to long-format from wide-format (including sales_training_evaluation.csv for result eval/compare)
-
-# TODO: rip out raw features and parse to sales_cleaned.parquet
-
-# TODO: next we want to engineer features out of the other csvs into the main sales_cleaned.parquet: price, day_of_week, special_event_1, special_event_2, lag_7, rolling_mean_7 and parse to sales_cleaned.parquet
+# TODO: next we want to engineer features out of the other csvs into the main sales_cleaned.csv: price, day_of_week, special_event_1, special_event_2, lag_7, rolling_mean_7 and parse to sales_cleaned.parquet
 
