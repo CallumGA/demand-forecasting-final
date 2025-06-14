f0b7a51afa294c587b713a3f9eead71dc8679023
callumanderson <callum.g.anderson@gmail.com>
2025-06-14 12:41:14 -0400

    preprocessing and feature engineering todos

--- src/feature_engineering_preprocessing.py
+++ src/feature_engineering_preprocessing.py
@@ -6,3 +6,12 @@ import pandas as pd
     ***********************************************
 """
 
+# TODO: unzip the raw data in data/raw/data.zip
+
+# TODO: we need to first sanitize the csv by making sure there are no empty rows/columns/cells
+
+# TODO: then we write to the cleaned final csv in data/processed/sales_cleaned.parquet
+
+# TODO: next we want to rip features out of the other csvs into the main sales_cleaned.parquet: price, day_of_week, is_holiday, lag_7, rolling_mean_7
+
+# TODO: finally we want to append the new feature rows to data/processed/sales_cleaned.parquet
