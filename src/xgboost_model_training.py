4a3d18d8ddac82f193691cb88098282d25f8fc31
callumanderson <callum.g.anderson@gmail.com>
2025-06-18 14:21:06 -0400

    setup training model - we must separate y(target) with x(features)

--- src/xgboost_model_training.py
+++ src/xgboost_model_training.py
@@ -1,7 +1,23 @@
+import os
 import pandas as pd
+import xgboost
 
 """
     ****************************************************************
      Train and optimize the model including hyper-parameter tuning.
     ****************************************************************
 """
+
+
+df = pd.read_csv(os.getenv("CLEANED_SALES_DATA"))
+
+# cast categorical columns for native handling
+df["item_id"] = df["item_id"].astype("category")
+df["store_id"] = df["store_id"].astype("category")
+
+# define target and feature columns
+y = df["sales"]
+X = df[[
+    "sell_price", "snap", "is_event_day", "lag_7", "rolling_mean_7",
+    "day_of_week", "price_change_pct", "month", "item_id", "store_id"
+]]
